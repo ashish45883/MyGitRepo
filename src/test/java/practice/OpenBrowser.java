20d6f86dd3f73ab75c124aca0be415aef2c6da8f
Ashish <ashis_pandit@gmail.com>
2025-01-26 21:43:03 +0530

    Added one test

--- src/test/java/practice/OpenBrowser.java
+++ src/test/java/practice/OpenBrowser.java
@@ -9,7 +9,17 @@ import org.testng.annotations.Test;
 public class OpenBrowser {
 	
 	@Test
-	public void launchBrowser() {
+	public void launchBrowser_1() {
+		WebDriver driver = new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com/");
+		System.out.println("Page Title : " + driver.getTitle());
+		driver.quit();
+	}
+	
+	@Test
+	public void launchBrowser_2() {
 		WebDriver driver = new ChromeDriver();	
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
