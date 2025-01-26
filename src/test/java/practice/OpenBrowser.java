9344cb64cd11e035751ae29cb9d5e2ea56e734c3
Ashish <ashis_pandit@gmail.com>
2025-01-26 21:34:36 +0530

    First Commit

---
+++ src/test/java/practice/OpenBrowser.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	@Test
+	public void launchBrowser() {
+		WebDriver driver = new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com/");
+		System.out.println("Page Title : " + driver.getTitle());
+		driver.quit();
+	}
+}
